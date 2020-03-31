10e1ab2823ac04820a9efb600e9f7c0d53450b0f
zesutom01 <zesu.achoir@gmail.com>
2020-03-31 23:20:12 +0800

    files modified

---
+++ resources/views/post.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts/app')
+
+@section('content')
+<div class="container">
+  <div class="col-md-8 col-md-offset-2">
+    <h1>{{$title}}</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach( $posts as $post)
+        <tr>
+          <td>{{$post->id}}</td>
+          <td>{{$post->title}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+  </div>
+
+</div>
+
+
+@endsection
