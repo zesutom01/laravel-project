1669d5ed1f0796d770bfbca45b7adb0e32e261d6
zesutom01 <zesu.achoir@gmail.com>
2020-04-01 02:59:59 +0800

    update user.blade

---
+++ resources/views/user.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">List All User</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Name</th>
+                          <th>Email </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                          <td>{{ $user->id }}</td>
+                          <td>{{ $user->name }}</td>
+                          <td>{{ $user->email }}</td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
