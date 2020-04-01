99f24ac601ead53e68ce64d9b5ca9115cd806508
zesutom01 <zesu.achoir@gmail.com>
2020-04-01 08:39:27 +0800

    renew chart

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class ChartController extends Controller
 {
@@ -13,7 +14,12 @@ class ChartController extends Controller
      */
     public function index()
     {
-        //
+      $users = User::select(\DB::raw("COUNT(*) as count"))
+                  ->whereYear('created_at', date('Y'))
+                  ->groupBy(\DB::raw("Month(created_at)"))
+                  ->pluck('count');
+
+      return view('chart', compact('users'));
     }
 
     /**
