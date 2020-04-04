1f1f9457f76e3ed1d4fc54a26c85e66e16e872e6
zesutom01 <zesu.achoir@gmail.com>
2020-04-04 15:26:56 +0800

    PostController public function modified

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         return Post::simplePaginate(10);
 
